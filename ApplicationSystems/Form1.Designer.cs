161e3cc881ce27f5c68f5fc1bf543b5a73474cde
Trofimenkova <anna.trof@tut.by>
2017-06-13 16:51:09 +0300

    I have developed the app for transformation the XML-file into the INI-file.

---
+++ ApplicationSystems/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ApplicationSystems
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.convert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // convert
+            // 
+            this.convert.Location = new System.Drawing.Point(62, 32);
+            this.convert.Name = "convert";
+            this.convert.Size = new System.Drawing.Size(213, 23);
+            this.convert.TabIndex = 0;
+            this.convert.Text = "Конвертировать XML-файл в INI";
+            this.convert.UseVisualStyleBackColor = true;
+            this.convert.Click += new System.EventHandler(this.convert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 93);
+            this.Controls.Add(this.convert);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Трансформация файла XML-INI";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button convert;
+    }
+}
+
---
+++ ApplicationSystems/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.IO;
+
+namespace ApplicationSystems
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convert_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:/users/user/documents";
+            openFileDialog1.Filter = "XML Files|*.xml";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                XDocument myXML = XDocument.Load(openFileDialog1.FileName);
+                IniFile.setPath(openFileDialog1.FileName);
+                IniFile.IniWriteValue(Convert.ToString(myXML.Root.Name), "text", myXML.Root.Value);
+                write_attributes(myXML.Root);
+                write_elements(myXML, myXML.Root);
+                IniFile.open();
+            }
+        }
+
+        private void write_elements(XDocument myXML, XElement e)
+        {
+            string[] sections = new string[100];
+            int kol = 0;
+            foreach (XElement el in myXML.Element(e.Name).Descendants())
+            {
+                sections[kol] = Convert.ToString(el.Name);
+                kol++;
+                int i = 0;
+                foreach (string s in sections)
+                {
+                    if (s != null && el.Name != null)
+                        if (s.Equals(Convert.ToString(el.Name))) i++;
+                }
+
+                if (i > 1) el.Name = el.Name + Convert.ToString(i);
+                IniFile.IniWriteValue(Convert.ToString(el.Name), "text", el.Value);
+                write_attributes(el);
+            }
+        }
+
+        private void write_attributes(XElement e)
+        {
+            foreach (XAttribute attr in e.Attributes())
+                IniFile.IniWriteValue(Convert.ToString(e.Name), Convert.ToString(attr.Name), Convert.ToString(attr.Value));
+        }
+
+        public class IniFile
+        {
+            private static string path = "c:/users/user/documents/";
+
+            public static void setPath(string file)
+            {
+                IniFile.path = IniFile.path + Path.GetFileNameWithoutExtension(file) + ".ini";
+            }
+
+            public static void open()
+            {
+                System.Diagnostics.Process.Start(path);
+            }
+
+            [DllImport("kernel32")]
+            private static extern long WritePrivateProfileString(string section,
+                string key, string val, string filePath);
+
+            public static void IniWriteValue(string Section, string Key, string Value)
+            {
+                WritePrivateProfileString(Section, Key, Value, path);
+            }
+        }
+    }
+}
